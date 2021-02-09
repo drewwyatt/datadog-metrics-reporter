b5e59e23254c34982826e7cc9b915cb2d954ad6d
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-02-08 21:50:07 -0500

    make workflow log the stuff josh is logging

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,44 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+const slugify = (s: string) =>
+  s
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/&/g, '-and-')
+    .replace(/[\s\W-]+/g, '-')
+    .replace(/-$/, '')
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.debug('Getting token')
+    const token = core.getInput('github-token...', { required: true })
+    core.debug('Getting octokit with token...')
+    const octokit = github.getOctokit(token)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const context = github.context
 
-    core.setOutput('time', new Date().toTimeString())
+    core.debug('listing jobs for workflow run...')
+    const currentRun = await octokit.actions.listJobsForWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId
+    })
+
+    core.debug('looping jobs...')
+    for (const job of currentRun.data.jobs) {
+      for (const step of job.steps) {
+        const completedAt: any = new Date(step.completed_at)
+        const startedAt: any = new Date(step.started_at)
+        const duration = (completedAt - startedAt) / 1000
+        core.debug(
+          `${slugify(job.name)}.${slugify(step.name)}.${
+            step.conclusion
+          }: ${duration}s`
+        )
+      }
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
