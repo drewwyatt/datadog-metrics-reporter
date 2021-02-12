31e638c1d1515ca7527073a3fcb3390c6c29ce1a
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-02-11 21:36:50 -0500

    use faker to generate fixtures

--- __tests__/fixtures.ts
+++ __tests__/fixtures.ts
@@ -1,44 +1,68 @@
+import faker from 'faker'
 import type { Context, Job } from '../src/utils'
 
-export const toContextFixture = (
-  overrides: Partial<Context> = {},
-): Context => ({
-  payload: {},
-  eventName: 'pull_request',
-  workflow: 'build_test',
-  sha: '123abc',
-  ref: 'main',
-  action: '',
-  actor: 'drewwyatt',
-  job: '',
-  runNumber: 123,
-  runId: 321,
-  issue: {
-    owner: 'drewwyatt',
-    repo: 'datadog_metrics_reporter',
-    number: 42,
-  },
-  repo: {
-    owner: 'drewwyatt',
-    repo: 'datadog_metrics_reporter',
-  },
-  ...overrides,
-})
+const toEventName = () =>
+  faker.random.arrayElement(['pull_request', 'push', 'schedule'] as const)
+const toName = () =>
+  faker.lorem.words(faker.random.number(5)).split(' ').join('_')
+const toConclusion = () =>
+  faker.random.arrayElement([
+    'action_required',
+    'cancelled',
+    'failure',
+    'neutral',
+    'success',
+    'skipped',
+    'stale',
+    'timed_out',
+  ] as const)
+const toId = () => faker.random.number(9999)
+const toStatus = () =>
+  faker.random.arrayElement(['queued', 'in_progress', 'completed'] as const)
+
+export const toContextFixture = (overrides: Partial<Context> = {}): Context => {
+  const owner = faker.internet.userName()
+  const repo = faker.random.words(faker.random.number(5))
+  return {
+    payload: {}, // ?
+    action: '', // ?
+    eventName: toEventName(),
+    workflow: toName(),
+    sha: faker.git.commitSha(),
+    ref: faker.git.branch(),
+    actor: faker.internet.userAgent(),
+    job: toName(),
+    runNumber: faker.random.number(9999),
+    runId: toId(),
+    issue: {
+      owner,
+      repo,
+      number: faker.random.number(9999),
+    },
+    repo: {
+      owner,
+      repo,
+    },
+    ...overrides,
+  }
+}
 
 export const toJobFixture = (overrides: Partial<Job> = {}): Job => ({
-  conclusion: 'completed',
-  id: 123,
-  check_run_url: 'https://check_run_url.fixture',
-  status: 'completed',
-  run_id: 321,
-  run_url: 'https://run_url.fixture',
-  node_id: 'some-node-id',
-  head_sha: '123abc',
+  conclusion: toConclusion(),
+  id: toId(),
+  check_run_url: faker.internet.url(),
+  status: toStatus(),
+  run_id: toId(),
+  run_url: faker.internet.url(),
+  node_id: faker.internet.userName(),
+  head_sha: faker.git.commitSha(),
   steps: [],
-  name: 'some-job-fixture',
-  url: 'https://url.fixture',
-  html_url: 'https://html_url.fixture',
+  name: toName(),
+  url: faker.internet.url(),
+  html_url: faker.internet.url(),
   started_at: '2021-02-11T06:36:05.147Z',
   completed_at: '2021-02-11T06:46:05.147Z',
   ...overrides,
 })
+
+export type { Context, Job }
