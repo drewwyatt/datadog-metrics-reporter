515f2edc741c6cc35461a1092cdbd8cb0690ead3
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-02-11 21:36:57 -0500

    validate metrics keys

--- __tests__/toMetricsReporter.test.ts
+++ __tests__/toMetricsReporter.test.ts
@@ -1,25 +1,44 @@
 import metrics from 'datadog-metrics'
 import { toMetricsReporter } from '../src/utils'
-import { toContextFixture, toJobFixture } from './fixtures'
+import { Context, Job, toContextFixture, toJobFixture } from './fixtures'
 
 jest.mock('datadog-metrics', () => ({
   gauge: jest.fn(),
   histogram: jest.fn(),
 }))
 
+const getMetricsParams = (
+  metricsFn: typeof metrics['gauge'] | typeof metrics['histogram'],
+  callIdx: 0 | 1,
+): [key: string, value: number, tags: string[]] =>
+  (metricsFn as jest.Mock).mock.calls[callIdx]
+
 describe('toMetricsReporter', () => {
   const logger = jest.fn()
   let subject: ReturnType<typeof toMetricsReporter>
+  let context: Context
+  let job: Job
 
   beforeEach(() => {
+    context = toContextFixture()
+    job = toJobFixture()
     ;(metrics.gauge as jest.Mock).mockReset()
     ;(metrics.histogram as jest.Mock).mockReset()
     ;(logger as jest.Mock).mockReset()
     subject = toMetricsReporter(logger, metrics)
   })
 
-  it('calls logger', () => {
-    subject(toContextFixture(), toJobFixture())
-    expect(logger).toHaveBeenCalled()
+  describe('for jobs', () => {
+    it('reports gauge metrics with the conclusion', () => {
+      subject(context, job)
+      const [metricsKey] = getMetricsParams(metrics.gauge, 0)
+      expect(metricsKey).not.toContain(job.conclusion)
+    })
+
+    it('reports gauge metrics without the conclusion', () => {
+      subject(context, job)
+      const [metricsKey] = getMetricsParams(metrics.gauge, 1)
+      expect(metricsKey).toContain(job.conclusion)
+    })
   })
 })
